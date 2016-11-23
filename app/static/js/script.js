82065f14b6f70a02081ebce2001b458a72c332ad
yingchao <yingchao.usc2013@gmail.com>
2016-11-22 23:30:52 -0800

    add the jquery file

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,8 +1,53 @@
 $(document).ready(function(){
+
+  //just a test here
+  polling();
   $('#send-button').click(function(){
     var toAdd = $('#inputbox').val();
     $('#conversation').before('<p style="color:blue">'+'<strong>I said:</strong> '+toAdd+'</p>');
     $('#inputbox').val('');
+    
+    var msg = {
+    	number: "0",
+    	content: "FROM ME: "+toAdd,
+    	//type = sms || fb
+    	type: "sms",
+    }
+    $.ajax({
+    	type: 'POST',
+    	url: '/api/sms_post',
+    	data: msg,
+    	success:function(json){
+    	    alert(json.result);
+    	}
+    });
   });
 })
+
+function populate_contact(data){
+
+}
+
+function polling(){
+    setTimeout(polling, 1000);
+    $.ajax({
+    	type: 'GET',
+    	url: '/api/contact_updates',
+    	success: function(data){
+    	    //$('#conversation').before('<p style="color:blue">'+'<strong>I said:</strong> '+'succeed'+'</p>');
+    	}
+    });
+}
+
+function retrieve_history(number){
+    $.ajax({
+    	type: 'GET',
+    	url: /api/message_history,
+    	data: number,
+    	success: function(json){
+    	
+    	}
+    });
+}
+
 
