07e953296a431f053727be981f8e0740f6911256
yingchao <yingchao.usc2013@gmail.com>
2016-11-30 18:00:03 -0800

    test

--- app/views.py
+++ app/views.py
@@ -165,12 +165,12 @@ def list_messages():
 
 @app.route('/messenger', methods=['GET','POST'])
 def messenger():
-    if request.methods == 'GET':
+    if request.method == 'GET':
         if (request.values.get('hub.verify_token') == 'abcd'):
             return request.values.get('hub.challenge')
         else:
             return 'Error, wrong validation token'
-    if request.methods == 'POST':
+    if request.method == 'POST':
         if request.data.get('object') == 'page':
             entry = request.data.get('entry')
             print entry
